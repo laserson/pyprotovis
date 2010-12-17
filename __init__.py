5e94001a04c73b53eb50195c0bb802b6f1f5c749
Uri Laserson <uri.laserson@gmail.com>
2010-12-17 00:54:17 -0500

    Added a couple helper fns to pv

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 import math
 
+import Color
+
 identity = lambda x: return x
 index = lambda: return self.index
 child = lambda: return self.childIndex
 parent = lambda: return self.parent.index
+
+color = Color.color
 
 def log(x,b=10):
     return math.log(x)/math.log(b)
@@ -13,3 +17,6 @@ def logFloor(x,b):
 
 def logCeil(x,b):
     return b**(math.ceil(log(x,b))) if x > 0 else -(b**(-math.ceil(-log(-x,b))))
+
+def numerate(x):
+    return dict([(d,i) for (i,d) in enumerate(x)])
